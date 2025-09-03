495b6a104bdb4f8b2ae89adc584b13720cafa280
Việt Anh <Vanhvu1701@gmail.com>
2025-09-03 12:00:20 +0700

    First commit

---
+++ baitap/src/App.js
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div>
+      <p>Mã sinh viên: 2210900089</p>
+      <p>Họ Và Tên: Vũ Việt Anh</p>
+      <p>Ngày sinh: 17/01/2004</p>
+    </div>
+  );
+}
+export default App;
+
